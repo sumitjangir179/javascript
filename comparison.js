5bce9028e96a77f93768e0aff10ba771d33760ea
sumitjangir179 <sumitjangir179@gmail.com>
2024-10-13 11:55:53 +0530

    comparison

---
+++ comparison.js
@@ -0,0 +1,39 @@
+// console.log(1 == 1) // true;
+// console.log(1 == 2) // false;
+// console.log(1 != 1) // false;
+// console.log(1 != 2) // true;
+// console.log(1 < 2) // true;
+// console.log(1 > 2) // false;
+// console.log(1 <= 2) // true;
+// console.log(1 >= 2) // false;
+
+// console.log('1' == 1) // true;
+// console.log('1' === 1) // false;
+
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true maybe due to conversion
+
+console.log(undefined == 0); // false
+console.log(undefined === 0); // false
+console.log(undefined > 0); // false
+console.log(undefined >= 0); // false
+
+
+
+
+
+
+/*
+
+== => equality
+=== => equality and type
+!= => not equal
+!== => not equal and type
+> => greater than
+< => less than
+>= => greater than or equal
+<= => less than or equal
+*/
+
