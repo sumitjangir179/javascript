6cb96ae803de99d35229a5249e7b71c69c6e11da
sumitjangir179 <sumitjangir179@gmail.com>
2024-10-13 10:54:10 +0530

    data types

---
+++ dataType.js
@@ -0,0 +1,33 @@
+const age = 18 
+const accountNumber = BigInt(123456789900987654321n)
+const name = 'sumit jangir'
+const isPresent = true
+let wish;
+const accountBalance = null
+
+console.log('age', typeof age)
+console.log('accountNumber', typeof accountNumber);
+console.log('name', typeof name);
+console.log('isPresent', typeof isPresent);
+console.log('wish', typeof wish); // undefined
+console.log('accountBalance', typeof accountBalance); // object
+
+/*
+primitive
+number - 2 power 53
+big int 
+string
+boolean -> true/false
+undefined 
+null => standalone value
+
+
+object
+
+*/
+
+
+
+
+
+
